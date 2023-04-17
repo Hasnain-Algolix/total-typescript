c6bfbfeb2cf4e84a3ef0b54594b1ef22e1ba0df2
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-04-17 17:00:29 +0500

    some extra code

--- index.ts
+++ index.ts
@@ -1,50 +1,93 @@
-// Import stylesheets
-import './style.css';
+// // Import stylesheets
+// import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-const hello = 'hello';
-console.log(hello);
+// // Write TypeScript code!
+// const appDiv: HTMLElement = document.getElementById('app');
+// appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// const hello = 'hello';
+// console.log(hello);
 
-type Role = 'admin' | 'user';
+// type Role = 'admin' | 'user';
 
-type ID = number | string;
+// type ID = number | string;
 
-interface Image {
-  id: ID;
-  link: string;
+// interface Image {
+//   id: ID;
+//   link: string;
+// }
+
+// interface Post {
+//   id: ID;
+//   title: string;
+//   description: string;
+//   images: Image[];
+//   publishDate: string;
+// }
+
+// interface User {
+//   id: ID;
+//   name: string;
+//   role: Role;
+//   posts: Array<Post>;
+//   isVerified: boolean;
+// }
+
+// const user: User = {
+//   id: 1,
+//   name: 'Stephen Grider',
+//   role: 'admin',
+//   posts: [
+//     {
+//       id: 1,
+//       title: 'Top 10 NPM packages for Node.js developers in 2023',
+//       description:
+//         'In this post we are gonna take a look at the most useful npm packages in 2023 to increase your productivity as a Node.js developer.',
+//       images: [{ id: 1, link: 'https://npm-packages.jpg' }],
+//       publishDate: '02-03-2023',
+//     },
+//   ],
+//   isVerified: true,
+// };
+
+// const guitarists = new Set();
+// const names = new Set<string>(['Mosh', 'Stephen']);
+
+// guitarists.add('Jimi Hendrix');
+// guitarists.add('Eric Clapton');
+
+// const map = new Map<string, string>();
+// map.set('name', 'Mosh');
+// console.log(map);
+
+// const langs = {};
+// langs['name'] = 'python';
+// console.log(langs);
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
 }
 
 interface Post {
-  id: ID;
+  id: string;
   title: string;
-  description: string;
-  images: Image[];
-  publishDate: string;
+  body: string;
 }
 
-interface User {
-  id: ID;
-  name: string;
-  role: Role;
-  posts: Array<Post>;
-  isVerified: boolean;
-}
+type UserPost = User & { posts: Post[] };
 
-const user: User = {
-  id: 1,
-  name: 'Stephen Grider',
-  role: 'admin',
-  posts: [
-    {
-      id: 1,
-      title: 'Top 10 NPM packages for Node.js developers in 2023',
-      description:
-        'In this post we are gonna take a look at the most useful npm packages in 2023 to increase your productivity as a Node.js developer.',
-      images: [{ id: 1, link: 'https://npm-packages.jpg' }],
-      publishDate: '02-03-2023',
-    },
-  ],
-  isVerified: true,
+export const getDefaultUserAndPosts = (): UserPost => {
+  return {
+    id: '1',
+    firstName: 'Matt',
+    lastName: 'Pocock',
+    posts: [
+      {
+        id: '1',
+        title: 'How I eat so much cheese',
+        body: "It's pretty edam difficult",
+      },
+    ],
+  };
 };
